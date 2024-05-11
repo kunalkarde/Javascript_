7824efbd5ef2e330fdc9d322d3b38265d35e0d3d
kunal karde <92689002+kunalkarde@users.noreply.github.com>
2024-05-11 08:59:50 +0000

    var const let understand

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 2318140
+let accountEmail="kunalkarde4@gmail.com"
+var accountPassword="Kunal123"
+accountCity = "Gondia"
+
+/*
+Use var when you need a variable with function or global scope.
+Use let when you need a variable with block-level scope and its value might change.
+Use const when you need a variable with block-level scope and its value won't change after initialization.
+
+*/
+
+
+// var
+
+function Example(){
+    var x=10;
+    if(true){
+        var y=20;
+
+    }
+    console.log(x);
+    console.log(y);
+}
+
+
