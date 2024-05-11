97202071a7ff6ebadc8be5e05f3901ac9fa3b431
kunal karde <92689002+kunalkarde@users.noreply.github.com>
2024-05-11 14:27:35 +0000

    datatypes  in javascript revision

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+
+
+// bigint
+//string
+//boolean -- true false
+//null   -- standalone value 
+//undefined -- 
+//Symbol 
+
+
+
+let num = 42; // Number
+let str = "Hello, world!"; // String
+let bool = true; // Boolean
+let undef; // Undefined
+let nul = null; // Null
+let sym = Symbol("foo"); // Symbol
+
+let obj = { key: "value" }; // Object
+
+function func() {
+    // Function
+        return "I'm a function!";
+        }
+
+let arr = [1, 2, 3]; // Array
+
+let date = new Date(); // Date
+
+let regex = /pattern/; // RegExp
